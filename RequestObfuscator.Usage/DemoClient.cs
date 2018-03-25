d105b8f4bf2c42db02e2329086926d7265d6702e
pawel.duszak <pawel.duszak@banqup.com>
2018-03-25 17:58:22 +0200

    Usage project.

---
+++ RequestObfuscator.Usage/DemoClient.cs
@@ -0,0 +1,27 @@
+﻿using RequestObfuscator.Client;
+using RequestObfuscator.Api;
+using RequestObfuscator.Enums;
+
+namespace RequestObfuscator.Usage
+{
+    public class DemoClient : ApiClient
+    {
+        public override IApiBuilder Configure(IApiBuilder api)
+        {
+            api.WithScheme("https")
+               .WithHost("reqres.in")
+               .RequestContentType(RequestContentTypeEnum.Json);
+
+            api.When()
+                .WithPath(path => path.Contains("/api/users/2"))
+                .MethodType(HttpMethodEnum.GET)
+                .BeginTamper()
+                .TamperFragment("test")
+                .TamperHeader("test", "test2")
+                .TamperMethodType(HttpMethodEnum.POST)
+                .TamperPath(path => path.Replace("/api/users/2", "/api/users/3"));
+
+            return api;
+        }
+    }
+}
---
+++ RequestObfuscator.Usage/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RequestObfuscator.Usage
+{
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            RequestObfuscator.Instance.Start();
+
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+    }
+}
